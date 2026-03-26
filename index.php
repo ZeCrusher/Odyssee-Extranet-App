414feaab52879857ef0d71576b5ec8a48c75eeb7
ZeCrusher <zecrusher@gmail.com>
2026-03-26 11:36:03 +0100

    Le fichier index de la première connexion

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+	tracage($_SERVER['PHP_SELF']);
+	header('Location: odyssee/authentication/sign-up/index.php');
+	exit();
+  
+	function tracage($texte) {
+		
+		// Si on peut déterminer l'adresse IP
+		$adresse_ip = Null;
+		if(isset($_SERVER['REMOTE_ADDR'])) {
+			$adresse_ip = '"'.$_SERVER['REMOTE_ADDR'].'"';
+		}
+		
+		$txt_log=$adresse_ip.';'.date('d/m/Y H:i:s').';'.$texte."\n";
+		// écriture dans un fichier de traçage
+		// $fichier = "log/tracage_".date('Ymd').".log";
+		
+		$fichier = "/log/tracage.log";
+		preg_match("`^(.*\/)([^\/]+)$`",$_SERVER['SCRIPT_FILENAME'], $matches);
+		$chemin_script = $matches[1];
+		$fichierCible = $chemin_script.$fichier;
+		$myFile=fopen($fichierCible,'a+');
+		fputs($myFile,$txt_log);
+		fclose($myFile);
+	}
+
+?>
