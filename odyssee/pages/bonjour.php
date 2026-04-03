0208388c872b8034dd7cc99f98be1c32ab7a13d4
ZeCrusher <zecrusher@gmail.com>
2026-04-03 14:18:12 +0200

    Ajout du fil d'Ariane

---
+++ odyssee/pages/bonjour.php
@@ -0,0 +1,26 @@
+		<div class="app-toolbar pt-5" id="kt_app_toolbar">
+			<div class="app-container container-fluid d-flex align-items-stretch" id="kt_app_toolbar_container">
+				<div class="app-toolbar-wrapper d-flex flex-stack flex-wrap gap-4 w-100">
+					<div class="page-title d-flex flex-column gap-1 me-3 mb-2">
+						<ul class="breadcrumb breadcrumb-separatorless fw-semibold mb-6">
+
+							<li class="breadcrumb-item text-gray-700 fw-bold lh-1">
+								<a class="text-gray-500" href="/odyssee/index.php"><i class="ki-duotone ki-home fs-3 text-gray-400 me-n1"></i></a>
+							</li>
+							<!--begin::Item-->
+						<!--	<li class="breadcrumb-item"><i class="ki-duotone ki-right fs-4 text-gray-700 mx-n1"></i></li><!--end::Item-->
+							<!--begin::Item-->
+							<!-- <li class="breadcrumb-item text-gray-700 fw-bold lh-1">Page d'accueil</li> ::Item-->
+							<!--begin::Item-->
+							<li class="breadcrumb-item"><i class="ki-duotone ki-right fs-4 text-gray-700 mx-n1"></i></li><!--end::Item-->
+							<li class="breadcrumb-item text-gray-700"><?php echo " ".$fil_ariane; ?></li><!--end::Item-->
+						</ul> 
+
+						<h1 class="page-heading d-flex flex-column justify-content-center text-dark fw-bolder fs-1 lh-0"><?php echo $grand_titre; ?></h1><!--end::Title-->
+					</div>
+					<!--begin::Actions-->
+					<!--	<a class="btn btn-sm btn-success ms-3 px-4 py-3" data-bs-target="#kt_modal_create_app" data-bs-toggle="modal" href="#">Bouton Project</a>  -->
+					<!--end::Actions-->
+				</div>
+			</div>
+		</div><!--end::Barre d'outils-->     
